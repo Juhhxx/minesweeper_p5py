8918ca73d08df9fde1a01242d2b0daa054accd08
Juhxx <juliacosta232005@gamil.com>
2023-11-14 20:02:09 +0000

    git commit -m "edited test"

--- test.py
+++ test.py
@@ -5,30 +5,32 @@ def setup():
     
     size(400,400)
           
-def detectMouseRelease(mouse_button):
+# def detectMouseRelease(mouse_button):
     
-    cliked = False
+#     cliked = False
     
-    mouse_presses.append(mouse_button)
+#     mouse_presses.append(mouse_button)
     
-    if len(mouse_presses) > 2:
+#     if len(mouse_presses) > 2:
         
-        mouse_presses.pop(0)
+#         mouse_presses.pop(0)
     
-        if mouse_presses[1] is not mouse_presses[0]:
+#         if mouse_presses[1] is not mouse_presses[0]:
             
-            cliked = True
+#             cliked = True
     
-    return cliked
+#     return cliked
+
+def mouse_released():
+    
+    print("aaaaa")
     
     
     
 def draw():
     
     
-    if detectMouseRelease(mouse_button):
-        
-        rect(mouse_x,mouse_y,20,20)
+    background(200,100,50)
         
     
     
